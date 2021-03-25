708c81101083332e62802602c421aa7ba9e85137
Shinigami92 <chrissi92@hotmail.de>
2021-03-25 19:21:16 +0100

    Add extends property to Override

--- src/overrides.d.ts
+++ src/overrides.d.ts
@@ -1,4 +1,5 @@
 import type { Environments } from './env';
+import type { Extends } from './extends';
 import type { Rules } from './rules';
 
 /**
@@ -6,11 +7,27 @@ import type { Rules } from './rules';
  */
 export interface Override {
   files: string[];
+  /**
+   * An environment provides predefined global variables.
+   *
+   * @see [Environments](https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments)
+   */
   env?: Environments;
+  /**
+   * Extending Configuration Files.
+   *
+   * @see [Extends](https://eslint.org/docs/user-guide/configuring/configuration-files#extending-configuration-files)
+   */
+  extends?: Extends;
+  /**
+   * Rules.
+   *
+   * @see [Rules](https://eslint.org/docs/user-guide/configuring/rules)
+   */
   rules?: Rules;
 }
 
 /**
- *
+ * Overrides.
  */
 export type Overrides = Array<Override>;
