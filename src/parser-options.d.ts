8221d144f7a6fc49e9196d8f8c16cca5fe692459
Shinigami92 <chrissi92@hotmail.de>
2021-03-25 16:50:50 +0100

    Improve ParserOptions

--- src/parser-options.d.ts
+++ src/parser-options.d.ts
@@ -1,10 +1,140 @@
+import { LiteralUnion } from './utility-types';
+
 /**
+ * Set to 3, 5 (default), 6, 7, 8, 9, 10, 11, or 12 to specify the version of ECMAScript syntax you want to use. You can also set to 2015 (same as 6), 2016 (same as 7), 2017 (same as 8), 2018 (same as 9), 2019 (same as 10), 2020 (same as 11), or 2021 (same as 12) to use the year-based naming.
  *
+ * @default 5
  */
-export interface ParserOptions {
-  ecmaVersion?: 2020;
+export type EcmaVersion = 3 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 2015 | 2016 | 2017 | 2018 | 2019 | 2020 | 2021;
+
+/**
+ * Set to "script" (default) or "module" if your code is in ECMAScript modules.
+ */
+export type SourceType = 'script' | 'module';
+
+/** Lib. */
+export type Lib = LiteralUnion<
+  | 'es5'
+  | 'es6'
+  | 'es2015'
+  | 'es7'
+  | 'es2016'
+  | 'es2017'
+  | 'es2018'
+  | 'es2019'
+  | 'es2020'
+  | 'esnext'
+  | 'dom'
+  | 'dom.iterable'
+  | 'webworker'
+  | 'webworker.importscripts'
+  | 'webworker.iterable'
+  | 'scripthost'
+  | 'es2015.core'
+  | 'es2015.collection'
+  | 'es2015.generator'
+  | 'es2015.iterable'
+  | 'es2015.promise'
+  | 'es2015.proxy'
+  | 'es2015.reflect'
+  | 'es2015.symbol'
+  | 'es2015.symbol.wellknown'
+  | 'es2016.array.include'
+  | 'es2017.object'
+  | 'es2017.sharedmemory'
+  | 'es2017.string'
+  | 'es2017.intl'
+  | 'es2017.typedarrays'
+  | 'es2018.asyncgenerator'
+  | 'es2018.asynciterable'
+  | 'es2018.intl'
+  | 'es2018.promise'
+  | 'es2018.regexp'
+  | 'es2019.array'
+  | 'es2019.object'
+  | 'es2019.string'
+  | 'es2019.symbol'
+  | 'es2020.bigint'
+  | 'es2020.promise'
+  | 'es2020.sharedmemory'
+  | 'es2020.string'
+  | 'es2020.symbol.wellknown'
+  | 'es2020.intl'
+  | 'esnext.array'
+  | 'esnext.symbol'
+  | 'esnext.asynciterable'
+  | 'esnext.intl'
+  | 'esnext.bigint'
+  | 'esnext.string'
+  | 'esnext.promise'
+  | 'esnext.weakref'
+  | 'es2016.full'
+  | 'es2017.full'
+  | 'es2018.full'
+  | 'es2019.full'
+  | 'es2020.full'
+  | 'esnext.full'
+  | 'lib'
+>;
+
+/** DebugLevel. */
+export type DebugLevel = boolean | Array<'eslint' | 'typescript' | 'typescript-eslint'>;
+
+/**
+ * Parser options.
+ *
+ * @see [Specifying Parser Options](https://eslint.org/docs/user-guide/configuring/language-options#specifying-parser-options)
+ */
+export interface ParserOptions extends Partial<Record<string, unknown>> {
+  /**
+   * Set to 3, 5 (default), 6, 7, 8, 9, 10, 11, or 12 to specify the version of ECMAScript syntax you want to use. You can also set to 2015 (same as 6), 2016 (same as 7), 2017 (same as 8), 2018 (same as 9), 2019 (same as 10), 2020 (same as 11), or 2021 (same as 12) to use the year-based naming.
+   *
+   * @default 5
+   */
+  ecmaVersion?: EcmaVersion;
+  /**
+   * Set to "script" (default) or "module" if your code is in ECMAScript modules.
+   */
+  sourceType?: SourceType;
+  /**
+   * An object indicating which additional language features you'd like to use.
+   */
+  ecmaFeatures?: {
+    /**
+     * Allow `return` statements in the global scope.
+     */
+    globalReturn?: boolean;
+    /**
+     * Enable global [strict mode](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode) (if `ecmaVersion` is 5 or greater).
+     */
+    impliedStrict?: boolean;
+    /**
+     * Enable [JSX](https://facebook.github.io/jsx/).
+     */
+    jsx?: boolean;
+  };
+  jsxPragma?: string;
+  jsxFragmentName?: string | null;
+  lib?: Lib[];
+  comment?: boolean;
+  debugLevel?: DebugLevel;
+  errorOnTypeScriptSyntacticAndSemanticIssues?: boolean;
+  errorOnUnknownASTType?: boolean;
+  extraFileExtensions?: string[];
+  filePath?: string;
+  loc?: boolean;
+  /**
+   * Parser.
+   *
+   * @see [Working with Custom Parsers](https://eslint.org/docs/developer-guide/working-with-custom-parsers)
+   * @see [Specifying Parser](https://eslint.org/docs/user-guide/configuring/plugins#specifying-parser)
+   */
   parser?: string;
-  project?: any;
-  sourceType?: 'module' | '...';
+  project?: string | string[];
+  projectFolderIgnoreList?: Array<string | RegExp>;
+  range?: boolean;
+  tokens?: boolean;
+  tsconfigRootDir?: string;
+  useJSXTextNode?: boolean;
   warnOnUnsupportedTypeScriptVersion?: boolean;
 }
