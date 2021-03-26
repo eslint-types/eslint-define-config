64b8ffcf70a73b263796eb8c533d2bfc5bcc665e
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2021-03-26 14:00:15 +0100

    chore: add linebreak-style rule

--- src/rules/eslint/index.d.ts
+++ src/rules/eslint/index.d.ts
@@ -1,5 +1,6 @@
 import type { CommaDangleRule } from './comma-dangle';
 import type { CurlyRule } from './curly';
+import type { LinebreakStyleRule } from './linebreak-style';
 import type { NoDebuggerRule } from './no-debugger';
 import type { QuotesRule } from './quotes';
 import type { SemiRule } from './semi';
@@ -7,4 +8,4 @@ import type { SemiRule } from './semi';
 /**
  * All eslint rules.
  */
-export type EslintRules = CommaDangleRule & CurlyRule & NoDebuggerRule & QuotesRule & SemiRule;
+export type EslintRules = CommaDangleRule & CurlyRule & LinebreakStyleRule & NoDebuggerRule & QuotesRule & SemiRule;
---
+++ src/rules/eslint/linebreak-style.d.ts
@@ -0,0 +1,32 @@
+import type { RuleConfig } from '../rule-config';
+
+/**
+ * Option.
+ */
+export type LinebreakStyleOption = 'unix' | 'windows';
+
+/**
+ * Options.
+ */
+export type LinebreakStyleOptions = [LinebreakStyleOption?];
+
+/**
+ * Enforce consistent linebreak style.
+ *
+ * @see [linebreak-style](https://eslint.org/docs/rules/linebreak-style)
+ */
+export type LinebreakStyleRuleConfig = RuleConfig<LinebreakStyleOptions>;
+
+/**
+ * Enforce consistent linebreak style.
+ *
+ * @see [linebreak-style](https://eslint.org/docs/rules/linebreak-style)
+ */
+export interface LinebreakStyleRule {
+  /**
+   * Enforce consistent linebreak style.
+   *
+   * @see [linebreak-style](https://eslint.org/docs/rules/linebreak-style)
+   */
+  'linebreak-style': LinebreakStyleRuleConfig;
+}
