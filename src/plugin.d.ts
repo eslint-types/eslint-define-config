daa42b2040159bcd36fb2f7e1117aaa892e0052f
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2021-03-26 13:55:51 +0100

    chore: add plugin inclusive-language

--- src/plugin.d.ts
+++ src/plugin.d.ts
@@ -1,4 +1,6 @@
 import type { LiteralUnion } from './utility-types';
 
 /** Plugin. */
-export type Plugin = LiteralUnion<'@typescript-eslint' | 'prettier' | 'import' | 'jsdoc' | 'spellcheck'>;
+export type Plugin = LiteralUnion<
+  '@typescript-eslint' | 'prettier' | 'import' | 'jsdoc' | 'spellcheck' | 'inclusive-language'
+>;
