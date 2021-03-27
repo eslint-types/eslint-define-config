602bc32ad90673ef7f4e6a4348ce62a6ee3bb14a
Shinigami92 <chrissi92@hotmail.de>
2021-03-27 16:01:03 +0100

    Add rule @typescript-eslint/interface-name-prefix

--- src/rules/typescript-eslint/index.d.ts
+++ src/rules/typescript-eslint/index.d.ts
@@ -1,7 +1,8 @@
 import type { BanTsCommentRule } from './ban-ts-comment';
 import type { ExplicitFunctionReturnTypeRule } from './explicit-function-return-type';
+import type { InterfaceNamePrefixRule } from './interface-name-prefix';
 
 /**
  * All @typescript-eslint rules.
  */
-export type TypeScriptEslintRules = BanTsCommentRule & ExplicitFunctionReturnTypeRule;
+export type TypeScriptEslintRules = BanTsCommentRule & ExplicitFunctionReturnTypeRule & InterfaceNamePrefixRule;
---
+++ src/rules/typescript-eslint/interface-name-prefix.d.ts
@@ -0,0 +1,7 @@
+import type { RuleConfig } from '../rule-config';
+
+/** @deprecated */
+export interface InterfaceNamePrefixRule {
+  /** @deprecated */
+  '@typescript-eslint/interface-name-prefix': RuleConfig;
+}
