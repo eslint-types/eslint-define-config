0e45a3e5c136d97c0a2873905519589995d0a6a0
Shinigami92 <chrissi92@hotmail.de>
2021-03-27 17:03:55 +0100

    Add rule @typescript-eslint/no-explicit-any

--- src/rules/typescript-eslint/index.d.ts
+++ src/rules/typescript-eslint/index.d.ts
@@ -2,6 +2,7 @@ import type { BanTsCommentRule } from './ban-ts-comment';
 import type { ExplicitFunctionReturnTypeRule } from './explicit-function-return-type';
 import type { InterfaceNamePrefixRule } from './interface-name-prefix';
 import type { MemberOrderingRule } from './member-ordering';
+import type { NoExplicitAnyRule } from './no-explicit-any';
 
 /**
  * All @typescript-eslint rules.
@@ -9,4 +10,5 @@ import type { MemberOrderingRule } from './member-ordering';
 export type TypeScriptEslintRules = BanTsCommentRule &
   ExplicitFunctionReturnTypeRule &
   InterfaceNamePrefixRule &
-  MemberOrderingRule;
+  MemberOrderingRule &
+  NoExplicitAnyRule;
---
+++ src/rules/typescript-eslint/no-explicit-any.d.ts
@@ -0,0 +1,45 @@
+import type { RuleConfig } from '../rule-config';
+
+/**
+ * Option.
+ */
+export type NoExplicitAnyOption = {
+  /**
+   * If `true`, auto-fixing will be made available in which the "any" type is converted to an "unknown" type.
+   *
+   * @default false
+   */
+  fixToUnknown: boolean;
+  /**
+   * Specify if arrays from the rest operator are considered okay.
+   *
+   * @default false
+   */
+  ignoreRestArgs: boolean;
+};
+
+/**
+ * Options.
+ */
+export type NoExplicitAnyOptions = [NoExplicitAnyOption?];
+
+/**
+ * Disallow usage of the `any` type.
+ *
+ * @see [no-explicit-any](https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-explicit-any.md)
+ */
+export type NoExplicitAnyRuleConfig = RuleConfig<NoExplicitAnyOptions>;
+
+/**
+ * Disallow usage of the `any` type.
+ *
+ * @see [no-explicit-any](https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-explicit-any.md)
+ */
+export interface NoExplicitAnyRule {
+  /**
+   * Disallow usage of the `any` type.
+   *
+   * @see [no-explicit-any](https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-explicit-any.md)
+   */
+  '@typescript-eslint/no-explicit-any': NoExplicitAnyRuleConfig;
+}
