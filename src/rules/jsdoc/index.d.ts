5ba2f5866d9939b895c81b2b4a50ec057feb8b33
Shinigami92 <chrissi92@hotmail.de>
2021-03-28 17:31:30 +0200

    Add rule jsdoc/require-returns-type

--- src/rules/jsdoc/index.d.ts
+++ src/rules/jsdoc/index.d.ts
@@ -2,8 +2,13 @@ import type { MatchDescriptionRule } from './match-description';
 import type { NoTypesRule } from './no-types';
 import type { RequireJsdocRule } from './require-jsdoc';
 import type { RequireParamTypeRule } from './require-param-type';
+import type { RequireReturnsTypeRule } from './require-returns-type';
 
 /**
  * All jsdoc rules.
  */
-export type JSDocRules = MatchDescriptionRule & NoTypesRule & RequireJsdocRule & RequireParamTypeRule;
+export type JSDocRules = MatchDescriptionRule &
+  NoTypesRule &
+  RequireJsdocRule &
+  RequireParamTypeRule &
+  RequireReturnsTypeRule;
---
+++ src/rules/jsdoc/require-returns-type.d.ts
@@ -0,0 +1,43 @@
+import type { RuleConfig } from '../rule-config';
+
+/**
+ * Option.
+ */
+export type RequireReturnsTypeOption = {
+  /**
+   * Set this to an array of strings representing the AST context where you wish the rule to be applied.
+   *
+   * Set to `"any"` if you want the rule to apply to any jsdoc block throughout your files (as is necessary for finding function blocks not attached to a function declaration or expression, i.e., `@callback` or `@function` (or its aliases `@func` or `@method`) (including those associated with an `@interface`).
+   *
+   * See the ["AST and Selectors"](https://github.com/gajus/eslint-plugin-jsdoc#eslint-plugin-jsdoc-advanced-ast-and-selectors) section of our README for more on the expected format.
+   *
+   * @see [contexts](https://github.com/gajus/eslint-plugin-jsdoc#contexts-3)
+   */
+  contexts?: string[];
+};
+
+/**
+ * Options.
+ */
+export type RequireReturnsTypeOptions = [RequireReturnsTypeOption?];
+
+/**
+ * Requires that `@returns` tag has `type` value.
+ *
+ * @see [require-returns-type](https://github.com/gajus/eslint-plugin-jsdoc#require-returns-type)
+ */
+export type RequireReturnsTypeRuleConfig = RuleConfig<RequireReturnsTypeOptions>;
+
+/**
+ * Requires that `@returns` tag has `type` value.
+ *
+ * @see [require-returns-type](https://github.com/gajus/eslint-plugin-jsdoc#require-returns-type)
+ */
+export interface RequireReturnsTypeRule {
+  /**
+   * Requires that `@returns` tag has `type` value.
+   *
+   * @see [require-returns-type](https://github.com/gajus/eslint-plugin-jsdoc#require-returns-type)
+   */
+  'jsdoc/require-returns-type': RequireReturnsTypeRuleConfig;
+}
