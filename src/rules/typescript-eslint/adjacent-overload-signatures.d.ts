7bcf4b184f6e4320623c8d22d6cba3162ff4bd84
Shinigami92 <chrissi92@hotmail.de>
2021-03-30 13:29:34 +0200

    Add rule @typescript-eslint/adjacent-overload-signatures

---
+++ src/rules/typescript-eslint/adjacent-overload-signatures.d.ts
@@ -0,0 +1,22 @@
+import type { RuleConfig } from '../rule-config';
+
+/**
+ * Require that member overloads be consecutive.
+ *
+ * @see [adjacent-overload-signatures](https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/adjacent-overload-signatures.md)
+ */
+export type AdjacentOverloadSignaturesRuleConfig = RuleConfig<[]>;
+
+/**
+ * Require that member overloads be consecutive.
+ *
+ * @see [adjacent-overload-signatures](https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/adjacent-overload-signatures.md)
+ */
+export interface AdjacentOverloadSignaturesRule {
+  /**
+   * Require that member overloads be consecutive.
+   *
+   * @see [adjacent-overload-signatures](https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/adjacent-overload-signatures.md)
+   */
+  '@typescript-eslint/adjacent-overload-signatures': AdjacentOverloadSignaturesRuleConfig;
+}
--- src/rules/typescript-eslint/index.d.ts
+++ src/rules/typescript-eslint/index.d.ts
@@ -1,3 +1,4 @@
+import type { AdjacentOverloadSignaturesRule } from './adjacent-overload-signatures';
 import type { BanTsCommentRule } from './ban-ts-comment';
 import type { ExplicitFunctionReturnTypeRule } from './explicit-function-return-type';
 import type { InterfaceNamePrefixRule } from './interface-name-prefix';
@@ -16,7 +17,8 @@ import type { TypedefRule } from './typedef';
 /**
  * All @typescript-eslint rules.
  */
-export type TypeScriptEslintRules = BanTsCommentRule &
+export type TypeScriptEslintRules = AdjacentOverloadSignaturesRule &
+  BanTsCommentRule &
   ExplicitFunctionReturnTypeRule &
   InterfaceNamePrefixRule &
   MemberOrderingRule &
