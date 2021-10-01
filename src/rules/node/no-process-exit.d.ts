39344faae5d73e3188c7567a98e9ec81e54fab53
Shinigami92 <chrissi92@hotmail.de>
2021-10-01 14:39:46 +0200

    Add options type

--- src/rules/node/no-process-exit.d.ts
+++ src/rules/node/no-process-exit.d.ts
@@ -1,11 +1,16 @@
 import type { RuleConfig } from '../rule-config';
 
 /**
+ * Options.
+ */
+export type NoProcessExitRuleOptions = [];
+
+/**
  * Disallow the use of `process.exit()`.
  *
  * @see [no-process-exit](https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-process-exit.md)
  */
-export type NoProcessExitRuleConfig = RuleConfig<[]>;
+export type NoProcessExitRuleConfig = RuleConfig<NoProcessExitRuleOptions>;
 
 /**
  * Disallow the use of `process.exit()`.
