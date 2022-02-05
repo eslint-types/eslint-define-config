4fa2dc68587967f81d2347dbc8b125f709b25349
Shinigami92 <chrissi92@hotmail.de>
2022-02-05 14:23:14 +0100

    Reconfigure renovate

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -7,4 +7,15 @@ module.exports = {
   plugins: [require.resolve('prettier-plugin-organize-imports')],
   singleQuote: true,
   trailingComma: 'all',
+  overrides: [
+    {
+      files: '*.json5',
+      options: {
+        parser: 'json5',
+        quoteProps: 'preserve',
+        singleQuote: false,
+        trailingComma: 'none',
+      },
+    },
+  ],
 };
