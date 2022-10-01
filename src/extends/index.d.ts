0ee80418c158428707edd0c15b491aaf2c9fcb0a
Julien <julien@ripouteau.com>
2022-10-01 23:31:58 +0200

    style: sort imports

--- src/extends/index.d.ts
+++ src/extends/index.d.ts
@@ -5,12 +5,12 @@ import type { JsdocExtensions } from './eslint-plugin-jsdoc';
 import type { MdxExtensions } from './eslint-plugin-mdx';
 import type { NodeExtensions } from './eslint-plugin-node';
 import type { PrettierExtensions } from './eslint-plugin-prettier';
+import type { SonarjsExtensions } from './eslint-plugin-sonarjs';
 import type { UnicornExtensions } from './eslint-plugin-unicorn';
 import type { VueExtensions } from './eslint-plugin-vue';
 import type { VuePugExtensions } from './eslint-plugin-vue-pug';
 import type { IntlifyVueI18nExtensions } from './intlify-vue-i18n';
 import type { TypescriptEslintExtensions } from './typescript-eslint';
-import type { SonarjsExtensions } from './eslint-plugin-sonarjs';
 
 /**
  * All known extensions.
