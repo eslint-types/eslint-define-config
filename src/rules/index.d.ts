a66e3856b1362f6d711a22e19173c020eadf4e9d
Julien <julien@ripouteau.com>
2022-10-01 20:31:05 +0200

    style: fix prettier error ?

--- src/rules/index.d.ts
+++ src/rules/index.d.ts
@@ -1,8 +1,8 @@
 import type { EslintRules } from './eslint';
 import type { ImportRules } from './import';
 import type { JSDocRules } from './jsdoc';
-import type { NodeRules } from './node';
 import type { NRules } from './n';
+import type { NodeRules } from './node';
 import type { RuleConfig } from './rule-config';
 import type { SpellcheckRules } from './spellcheck';
 import type { TypeScriptRules } from './typescript-eslint';
