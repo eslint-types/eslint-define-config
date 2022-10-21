aa5263b7bbcc02c174edaca7023d90a45c23ba30
三咲智子 <sxzz@sxzz.moe>
2022-10-21 15:17:54 +0800

    feat: add mjs

--- src/index.mjs
+++ src/index.mjs
@@ -7,3 +7,15 @@
 export function defineConfig(config) {
   return config;
 }
+
+/**
+ * Define a flat ESLint config.
+ *
+ * @param config flat ESLint config.
+ * @returns flat ESLint config.
+ *
+ * @see [Configuration Files (New)](https://eslint.org/docs/latest/user-guide/configuring/configuration-files-new)
+ */
+export function defineFlatConfig(config) {
+  return config;
+}
