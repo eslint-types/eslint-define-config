8b86b206a0ff994ed3900fd5615c51d8b5bf4935
Shinigami92 <chrissi92@hotmail.de>
2022-10-23 11:41:37 +0200

    No changes in mdx rules

--- src/rules/mdx/remark.d.ts
+++ src/rules/mdx/remark.d.ts
@@ -3,19 +3,16 @@ import type { RuleConfig } from '../rule-config';
 /**
  * Linter integration with remark plugins.
  *
- *
  */
 export type RemarkRuleConfig = RuleConfig<[]>;
 
 /**
  * Linter integration with remark plugins.
  *
- *
  */
 export interface RemarkRule {
   /**
    * Linter integration with remark plugins.
-   *
    *
    */
   'mdx/remark': RemarkRuleConfig;
