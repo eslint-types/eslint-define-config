b16c71244d8482f76c189d5ed9728e4d44cbd529
zz <2418184580@qq.com>
2023-01-02 19:55:45 +0800

    fix: update `excludedFiles` type (#159)
    Co-authored-by: Shinigami <chrissi92@hotmail.de>

--- src/config/overrides.d.ts
+++ src/config/overrides.d.ts
@@ -11,7 +11,11 @@ import type { Settings } from './settings';
 export interface Override {
   files: string[] | string;
 
-  excludedFiles?: string;
+  /**
+   * The glob patterns for excluded files.
+   */
+  // https://github.com/eslint/eslint/blob/762a8727fb3b5619cff900826053b643ca5f1162/lib/shared/types.js#L59
+  excludedFiles?: string | string[];
 
   /**
    * An environment provides predefined global variables.
