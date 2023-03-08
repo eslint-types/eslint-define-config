50b874f70b193d518bf383644e4caf67b71c1bed
Shinigami92 <chrissi92@hotmail.de>
2023-03-08 18:00:46 +0100

    Sync to eslint Linter.Rule types

--- src/rules/rule-config.d.ts
+++ src/rules/rule-config.d.ts
@@ -1,8 +1,31 @@
-import type { RuleSeverity } from './rule-severity';
+import type { RuleLevel } from './rule-severity';
+
+// Synced to https://github.com/DefinitelyTyped/DefinitelyTyped/blob/042141ce5f77f36df01c344ad09f32feda26c4fd/types/eslint/helpers.d.ts#L1-L3
+
+export type Prepend<Tuple extends any[], Addend> = ((
+  _: Addend,
+  ..._1: Tuple
+) => any) extends (..._: infer Result) => any
+  ? Result
+  : never;
+
+// Synced to https://github.com/DefinitelyTyped/DefinitelyTyped/blob/042141ce5f77f36df01c344ad09f32feda26c4fd/types/eslint/index.d.ts#L717-L719
 
 /**
  * Rule configuration.
  */
-export type RuleConfig<Options extends unknown[] = unknown[]> =
-  | RuleSeverity
-  | [RuleSeverity, ...Options];
+export type RuleLevelAndOptions<Options extends any[] = any[]> = Prepend<
+  Partial<Options>,
+  RuleLevel
+>;
+
+export type RuleEntry<Options extends any[] = any[]> =
+  | RuleLevel
+  | RuleLevelAndOptions<Options>;
+
+/**
+ * Rule configuration.
+ *
+ * @alias RuleEntry
+ */
+export type RuleConfig<Options extends any[] = any[]> = RuleEntry<Options>;
--- src/rules/rule-severity.d.ts
+++ src/rules/rule-severity.d.ts
@@ -1,4 +1,18 @@
+// Synced to https://github.com/DefinitelyTyped/DefinitelyTyped/blob/042141ce5f77f36df01c344ad09f32feda26c4fd/types/eslint/index.d.ts#L714-L716
+
+/**
+ * Rule ordinal severity.
+ */
+export type Severity = 0 | 1 | 2;
+
 /**
  * Rule severity.
  */
-export type RuleSeverity = 'off' | 'warn' | 'error' | 0 | 1 | 2;
+export type RuleLevel = Severity | 'off' | 'warn' | 'error';
+
+/**
+ * Rule severity.
+ *
+ * @alias RuleLevel
+ */
+export type RuleSeverity = RuleLevel;
