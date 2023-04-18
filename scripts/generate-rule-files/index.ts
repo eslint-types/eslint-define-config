8633c520ba26b870fffb4a9af3c4d6565c346b4b
Dimava <dimava2@ya.ru>
2023-04-18 15:48:18 +0300

    fix: log prettier loading

--- scripts/generate-rule-files/index.ts
+++ scripts/generate-rule-files/index.ts
@@ -158,6 +158,8 @@ export async function run(options: RunOptions = {}): Promise<void> {
     }
 
     logger.info(`Generating ${plugin.name} rules.`);
+    logger.logUpdate(logger.colors.yellow('  Loading plugin > Prettier'));
+    format('');
     logger.logUpdate(
       logger.colors.yellow(`  Loading plugin > ${plugin.module}`),
     );
