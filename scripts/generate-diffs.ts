d3dbf1dcdf3d924f1788e10704437b4f46dac7d1
Dimava <dimava2@ya.ru>
2023-04-18 16:58:03 +0300

    feat: generate:diffs command

---
+++ scripts/generate-diffs.ts
@@ -0,0 +1,12 @@
+import { execSync } from 'node:child_process';
+const changes: string[] = execSync('git diff --name-only --staged', {
+  encoding: 'utf-8',
+}).split('\n');
+
+const rules: string[] = changes.filter((c) => c.startsWith('src/rules/'));
+console.log(rules);
+
+for (const rule of rules) {
+  const diffPath: string = `scripts/generate-rule-files/diffs/${rule.slice(4)}`;
+  execSync(`git diff --staged ${rule} > ${diffPath}.diff`);
+}
