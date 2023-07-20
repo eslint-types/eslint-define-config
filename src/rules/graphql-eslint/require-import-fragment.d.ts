7f63527b0e4292dfa9c630beed950d2f8f1c875a
Shinigami92 <chrissi92@hotmail.de>
2023-07-20 15:09:02 +0200

    Update graphql-eslint rules

--- src/rules/graphql-eslint/require-import-fragment.d.ts
+++ src/rules/graphql-eslint/require-import-fragment.d.ts
@@ -3,20 +3,20 @@ import type { RuleConfig } from '../rule-config';
 /**
  * Require fragments to be imported via an import expression.
  *
- * @see [require-import-fragment](https://github.com/B2o5T/graphql-eslint/blob/master/docs/rules/require-import-fragment.md)
+ * @see [require-import-fragment](https://the-guild.dev/graphql/eslint/rules/require-import-fragment)
  */
 export type RequireImportFragmentRuleConfig = RuleConfig<[]>;
 
 /**
  * Require fragments to be imported via an import expression.
  *
- * @see [require-import-fragment](https://github.com/B2o5T/graphql-eslint/blob/master/docs/rules/require-import-fragment.md)
+ * @see [require-import-fragment](https://the-guild.dev/graphql/eslint/rules/require-import-fragment)
  */
 export interface RequireImportFragmentRule {
   /**
    * Require fragments to be imported via an import expression.
    *
-   * @see [require-import-fragment](https://github.com/B2o5T/graphql-eslint/blob/master/docs/rules/require-import-fragment.md)
+   * @see [require-import-fragment](https://the-guild.dev/graphql/eslint/rules/require-import-fragment)
    */
   '@graphql-eslint/require-import-fragment': RequireImportFragmentRuleConfig;
 }
--- src/rules/graphql-eslint/require-nullable-result-in-root.d.ts
+++ src/rules/graphql-eslint/require-nullable-result-in-root.d.ts
@@ -3,20 +3,20 @@ import type { RuleConfig } from '../rule-config';
 /**
  * Require nullable fields in root types.
  *
- * @see [require-nullable-result-in-root](https://github.com/B2o5T/graphql-eslint/blob/master/docs/rules/require-nullable-result-in-root.md)
+ * @see [require-nullable-result-in-root](https://the-guild.dev/graphql/eslint/rules/require-nullable-result-in-root)
  */
 export type RequireNullableResultInRootRuleConfig = RuleConfig<[]>;
 
 /**
  * Require nullable fields in root types.
  *
- * @see [require-nullable-result-in-root](https://github.com/B2o5T/graphql-eslint/blob/master/docs/rules/require-nullable-result-in-root.md)
+ * @see [require-nullable-result-in-root](https://the-guild.dev/graphql/eslint/rules/require-nullable-result-in-root)
  */
 export interface RequireNullableResultInRootRule {
   /**
    * Require nullable fields in root types.
    *
-   * @see [require-nullable-result-in-root](https://github.com/B2o5T/graphql-eslint/blob/master/docs/rules/require-nullable-result-in-root.md)
+   * @see [require-nullable-result-in-root](https://the-guild.dev/graphql/eslint/rules/require-nullable-result-in-root)
    */
   '@graphql-eslint/require-nullable-result-in-root': RequireNullableResultInRootRuleConfig;
 }
