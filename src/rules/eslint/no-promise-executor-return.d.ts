11ecc5a74eb6d44bc733ed3fac68fe5b83f31e88
Shinigami92 <chrissi92@hotmail.de>
2023-09-22 20:58:27 +0200

    Add eslint rules

--- src/rules/eslint/no-promise-executor-return.d.ts
+++ src/rules/eslint/no-promise-executor-return.d.ts
@@ -1,11 +1,24 @@
 import type { RuleConfig } from '../rule-config';
 
 /**
+ * Option.
+ */
+export interface NoPromiseExecutorReturnOption {
+  allowVoid?: boolean;
+}
+
+/**
+ * Options.
+ */
+export type NoPromiseExecutorReturnOptions = [NoPromiseExecutorReturnOption?];
+
+/**
  * Disallow returning values from Promise executor functions.
  *
  * @see [no-promise-executor-return](https://eslint.org/docs/latest/rules/no-promise-executor-return)
  */
-export type NoPromiseExecutorReturnRuleConfig = RuleConfig<[]>;
+export type NoPromiseExecutorReturnRuleConfig =
+  RuleConfig<NoPromiseExecutorReturnOptions>;
 
 /**
  * Disallow returning values from Promise executor functions.
